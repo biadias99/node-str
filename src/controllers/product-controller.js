8318f98ce9943280f6e04701a26bd10525d7bffc
bianca.barbosa <biadiasbarbosa@hotmail.com>
2018-09-29 21:27:15 -0300

    #5 Gets, update, delete

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -14,6 +14,44 @@ exports.get = (req, res, next) => {
 
 }
 
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({ 
+            slug: req.params.slug,
+            active: true 
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);  
+        }).catch(e =>{
+            res.status(400).send(e);  
+        });
+
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);  
+        }).catch(e =>{
+            res.status(400).send(e);  
+        });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);  
+        }).catch(e =>{
+            res.status(400).send(e);  
+        });
+
+}
+
 exports.post = (req, res, next) => {
     var product = new Product(req.body); // salva td sem validação
     // var product = new Product();
@@ -26,15 +64,29 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body
-    });// to enviando uma resposta
+    // poderia usar save também
+    Product
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price,
+                slug: req.body.slug
+            }
+        })
+        .then(x => {
+            res.status(200).send({ message: 'Produto atualizado com sucesso!' });  
+        }).catch(e =>{
+            res.status(400).send({ message: 'Falha ao atualizar o produto', data: e });  
+        });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(
-        req.body
-    );// to enviando uma resposta  
+    Product
+        .findOneAndRemove(req.body.id)
+        .then(x => {
+            res.status(200).send({ message: 'Produto removido com sucesso!' });  
+        }).catch(e =>{
+            res.status(400).send({ message: 'Falha ao remover o produto', data: e });  
+        }); 
 };
